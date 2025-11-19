3ce2a92857ab9a5e67ebe9c37681405473324295
Andrii Posternak <posternak.andrii1@gmail.com>
2025-11-19 12:03:51 +0200

    Use the commandeer package in the Tester class instead of local "cli abstraction" solutions.

--- src/Core/Tester.php
+++ src/Core/Tester.php
@@ -2,12 +2,12 @@
 
 namespace RectorIntegrationTool\Core;
 
-use RectorIntegrationTool\Core\CliAbstraction\Artisan;
-use RectorIntegrationTool\Core\CliAbstraction\ShellCommand;
+use Posternak\Commandeer\Builders\Artisan;
+use Posternak\Commandeer\ShellCommand;
 
 final class Tester {
     static public function test(string $projectType): ?ShellCommand {
-        if ($projectType === 'laravel') return Artisan::test();
+        if ($projectType === 'laravel') return Artisan::test()->run();
         elseif ($projectType === 'package') return new ShellCommand('php ./vendor/bin/codecept run')->run();
 
         return null;
